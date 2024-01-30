30d01df8fc61508e7ef837b1df75ec04a706dcfe
Jiang-Feiyu <msjfy223@connect.hku.hk>
2024-01-30 17:06:42 +0800

    integrate with AI model - wav figure finished

--- backend/Interpret.py
+++ backend/Interpret.py
@@ -23,6 +23,8 @@ def import_packages():
     from colorama import Fore
     import numpy as np
     import concurrent.futures
+    import soundfile as sf
+    from scipy.signal import resample
 
     # TensorFlow 和 Keras
     import tensorflow as tf
@@ -32,10 +34,10 @@ def import_packages():
     import h5py
 
     # 返回需要的模块
-    return sys, os, pathlib, pickle, datetime, wave, listdir, isfile, join, randint, re, librosa, plt, imgshow, np, wavfile, write, display, Fore, tf, load_model, h5py, concurrent
+    return sys, os, pathlib, pickle, datetime, wave, listdir, isfile, join, randint, re, librosa, plt, imgshow, np, wavfile, write, display, Fore, tf, load_model, h5py, concurrent, sf, resample
 
 # 调用函数来导入所有包
-sys, os, pathlib, pickle, datetime, wave, listdir, isfile, join, randint, re, librosa, plt, imgshow, np, wavfile, write, display, Fore, tf, load_model, h5py, concurrent = import_packages()
+sys, os, pathlib, pickle, datetime, wave, listdir, isfile, join, randint, re, librosa, plt, imgshow, np, wavfile, write, display, Fore, tf, load_model, h5py, concurrent, sf, resample = import_packages()
 
 # 检验h5文件是否损坏
 def check_hdf5_file(file_path):
@@ -83,6 +85,39 @@ def load_models_and_pickles(model_paths, pickle_paths):
         pickle_data = list(executor.map(load_pickle_file, pickle_paths))
     
     return models, pickle_data
+
+# A function to read an audio .wav file and shows the waveform
+def showAudio(file_name, SHOW):
+    waveform1 = []  # 初始化波形数组
+    print("Speech_Recognition.py: showAudio() version March 10,2023 ")  # 打印版本信息
+    audio_binary = tf.io.read_file(file_name)  # 读取音频文件的二进制数据
+    audio, sampleR = tf.audio.decode_wav(audio_binary)  # 解码音频文件，并获取音频数据和采样率
+    print("Speech_Recognition.py: showAudio() sampleR = ", sampleR)  # 打印采样率信息
+
+    if (sampleR != 48000):  # 如果采样率不是48000（标准采样率）
+        print("showAudio(): Conversion is needed. To start now.")  # 打印提示信息，需要进行转换
+        yy, sr = sf.read(file_name)
+        yy_resampled = resample(yy, int(len(yy) * (48000 / sr))) # 对音频数据进行采样率转换为48000Hz
+        sf.write(file_name, yy_resampled, 48000)
+        audio_binary = tf.io.read_file(file_name)  # 重新读取转换后的音频文件
+        audio, sampleR = tf.audio.decode_wav(audio_binary)  # 解码转换后的音频文件
+        print("showAudio(): Conversion is completed.")  # 打印转换完成的提示信息
+
+    waveform2 = audio.numpy()  # 将音频数据转换为numpy数组
+    if (waveform2.shape[1] == 1):  # 如果音频为单声道
+        waveform1 = waveform2  # 直接将音频数据赋值给波形数组
+    else:  # 如果音频为立体声
+        waveform1 = waveform2[:, 1]  # 取出立体声音频中的右声道，转换为单声道
+        
+    if SHOW:  # 如果需要显示波形图
+        plt.figure(figsize=(25, 15), dpi=40)  # 设置图像大小和分辨率
+        plt.plot(waveform1)  # 绘制波形图
+        plt.xlabel('Sample')  # 设置x轴标签
+        plt.ylabel('Amplitude')  # 设置y轴标签
+        plt.title('Waveform')  # 设置图像标题
+        plt.show()  # 显示图像
+    
+    return waveform1  # 返回处理后的波形数据数组
 
 # 主函数 - 文字化输入内容
 def execute_interpret(user_id: str, file_index: int, model_id: int):
@@ -132,4 +167,8 @@ def execute_interpret(user_id: str, file_index: int, model_id: int):
     models, pickle_data = load_models_and_pickles(model_paths, pickle_paths)
 
 # 主函数测试入口
-execute_interpret("ABC@abc.com", 1, 3)
+# execute_interpret("ABC@abc.com", 1, 3)
+
+# 函数调试
+file_name = "./Data/2.wav"
+waveform_data = showAudio(file_name, True)  # 将波形图显示出来
